dea93e72cca0b40b670fc29a2c5bf7916dc47222
Trevor Alt <trevorjalt@gmail.com>
2020-09-28 14:39:42 -0400

    pushing

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
+const usersRouter = require('./users/users-router')
+const commentsRouter = require('./comments/comments-router')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'dev'
-  : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
+app.use('/api/articles', articlesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/comments', commentsRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error: { messages: 'server error' } };
-    } else {
-        console.error(error)
-        response = { message: error.message, error };
-    }
-    res.status(500).json(response);
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
