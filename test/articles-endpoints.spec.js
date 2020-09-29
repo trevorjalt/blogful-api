4d4cf83b2313afdeb6f5dad411b7b0a1b885d4b8
Trevor Alt <trevorjalt@gmail.com>
2020-09-28 22:54:34 -0400

    prepared for deployment

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -10,7 +10,8 @@ describe('Articles Endpoints', function() {
 
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      // connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
 
